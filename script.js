c20bbefcacfaf32f9d0f8df8b53376c9c4c52675
antunezFernando <antunez.fernandoivan.43377@gmail.com>
2024-10-25 17:44:43 -0300

    Add form to ad new book

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ const myLibrary = [];
 let bookContainer = document.querySelector("#book-container");
 
 let newBookButton = document.querySelector("#new-book-button");
+let newBookClose = document.querySelector("#new-book-close");
+let newBookModal = document.querySelector("#new-book-modal");
 newBookButton.addEventListener("click", () => {
-    addBookToLibrary("Hi", "hello", 200, false);
-    renderBooks();
+    newBookModal.classList.toggle("modal-hide");
+});
+newBookClose.addEventListener("click", () => {
+    newBookModal.classList.toggle("modal-hide");
+});
+
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", (e) => {
+    handleNewBookForm();
+    e.preventDefault();
+});
+
+let creditsModalOpen = document.querySelector("#credits-open");
+let creditsModalClose = document.querySelector("#credits-close");
+let creditsModal = document.querySelector("#credits-container");
+creditsModalOpen.addEventListener("click", () => {
+    creditsModal.classList.toggle("modal-hide");
+});
+
+creditsModalClose.addEventListener("click", () => {
+    creditsModal.classList.toggle("modal-hide");
 });
 
 function Book(title, author, pages, read) {
@@ -32,6 +53,33 @@ function createInitialBooks() {
     }
 }
 
+function handleNewBookForm() {
+    let titleInput = document.querySelector("#title");
+    let authorInput = document.querySelector("#author");
+    let pagesInput = document.querySelector("#pages");
+    let readInput = document.querySelector("#read");
+
+    if(!titleInput.checkValidity() || !authorInput.checkValidity() || !pagesInput.checkValidity()) {
+        return;
+    }
+
+    let title = titleInput.value;
+    titleInput.value = "";
+
+    let author = authorInput.value;
+    authorInput.value = "";
+
+    let pages = pagesInput.value;
+    pagesInput.value = "";
+
+    let read = readInput.checked;
+    readInput.checked = false;
+
+    addBookToLibrary(title, author, pages, read);
+    renderBooks();
+    newBookModal.classList.toggle("modal-hide");
+}
+
 function createBookCardElement(book) {
     let bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
@@ -42,6 +90,9 @@ function createBookCardElement(book) {
 
     let author = document.createElement("p");
     author.innerText = book.author;
+
+    let pages = document.createElement("p");
+    pages.innerText = book.pages;
 
     let read = document.createElement("p");
     read.innerText = book.read ? "Read" : "Not read";
@@ -60,6 +111,7 @@ function createBookCardElement(book) {
 
     bookCard.appendChild(title);
     bookCard.appendChild(author);
+    bookCard.appendChild(pages)
     bookCard.appendChild(read);
     bookCard.appendChild(toggleReadButton);
     bookCard.appendChild(deleteBookButton);
